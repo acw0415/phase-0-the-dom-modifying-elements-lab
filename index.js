08cec0bc1ea8e082d61cb5a9d481493c0d34d34e
Austin <acw0415@gmail.com>
2022-06-28 21:22:05 -0400

    commit tree 4fe1079c13f54cb6aa650407c801e81ad678b680

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const elem = document.querySelector('body');
+elem.removeChild(main);
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory")
+const text = document.createTextNode("<h1>YOUR-NAME is the champion!</h1>");
+newHeader.appendChild(text);
+document.getElementById('h1').id = 'victory'
